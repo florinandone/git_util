ed8cf838594cc8faef8faa94e43e64b23704d0cc
Florin Andone <florinandone@outlook.com>
2023-09-24 20:44:59 +1000

    fix branch.properties

--- branch.sh
+++ branch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Define default work_folder
-work_folder="$(pwd)"  # Set work_folder to the current working directory
+# Define default paths and sub-folder
+work_folder="$(pwd)"        # Set work_folder to the current working directory
+clone_folder="$work_folder/clone"  # Define the clone sub-folder
+project_list_properties="$work_folder/project_list.properties"
 
 # Define properties files within work_folder
 project_list_properties="$work_folder/project_list.properties"
@@ -28,7 +30,7 @@ branch_name=$(grep '^current_branch=' "$branch_properties" | cut -d'=' -f2)
 # Read the project names from project_list.properties
 while read -r project; do
   # Navigate to the project folder within work_folder
-  cd "$work_folder/$project" || exit 1
+  cd "$clone_folder/$project" || exit 1
 
   # Ensure we're on the specified base branch
   git checkout "$base_branch"
@@ -36,7 +38,7 @@ while read -r project; do
   # Create a new branch with the branch name
   git checkout -b "$branch_name"
 
-  # Return to the script's folder (work_folder)
+  # Return to the script's folder (clone_folder)
   cd - || exit 1
 
   echo "Created branch '$branch_name' for project '$project'"
